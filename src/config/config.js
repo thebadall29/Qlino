d02bc21356b3abdd13c82c6feedf50d2326220a5
thebadall29 <thebadall29@gmail.com>
2025-08-06 18:10:28 +0530

    Update config.js
    change the url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+    API_URL: 'https://qlino.onrender.com' || 'http://localhost:5000',
 };
 
 export default config;
